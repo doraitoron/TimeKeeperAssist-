c9b2a03c2f414821539fd7d90066567f7748bd8d
憲和　辰馬 <cl_s3206541025@soshi.jp>
2022-10-12 14:08:18 +0900

    Initial Commit

---
+++ TimeKeeperAssist Ⅱ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimeKeeperAssist Ⅱ
+//
+//  Created by clark on 2022/10/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
